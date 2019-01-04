2bac79f65735ed1232ab89bd7c7be4ee1f36c5c9
along <wuhl6@mail2.sysu.edu.cn>
2019-01-04 19:48:30 +0800

    fix two bugs in lab1_result: 1. failed to ld bootblock because it exceeds 512 bytes; 2. failed to run kernel because it memset some memory which is read-only.

--- labcodes_answer/lab1_result/kern/init/init.c
+++ labcodes_answer/lab1_result/kern/init/init.c
@@ -16,7 +16,7 @@ static void lab1_switch_test(void);
 void
 kern_init(void){
     extern char edata[], end[];
-    memset(edata, 0, end - edata);
+    memset(0x10fac0, 0, end - 0x10fac0);
 
     cons_init();                // init the console
 
